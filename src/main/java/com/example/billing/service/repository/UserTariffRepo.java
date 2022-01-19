f54a931946eed1caaffae167b15c83061406d83b
komarov-se <komarov-se@rcbi.pro>
2022-01-19 21:48:44 +0300

    use List instead of Iterable

--- src/main/java/com/example/billing/service/repository/UserTariffRepo.java
+++ src/main/java/com/example/billing/service/repository/UserTariffRepo.java
@@ -3,6 +3,8 @@ package com.example.billing.service.repository;
 import com.example.billing.service.entity.UserTariffEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserTariffRepo extends CrudRepository<UserTariffEntity, Long> {
-    Iterable<UserTariffEntity> findByUserId(Long userId);
+    List<UserTariffEntity> findByUserId(Long userId);
 }
