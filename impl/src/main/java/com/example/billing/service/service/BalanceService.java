963f53ed483e2d9380ed2802bc600de775426a0d
komarov-se <komarov-se@rcbi.pro>
2022-01-27 19:22:23 +0300

    use schedule

--- impl/src/main/java/com/example/billing/service/service/BalanceService.java
+++ impl/src/main/java/com/example/billing/service/service/BalanceService.java
@@ -1,24 +1,25 @@
 package com.example.billing.service.service;
+
+import com.example.billing.service.entity.UserEntity;
 import com.example.billing.service.entity.UserTariffEntity;
 import com.example.billing.service.repository.UserRepo;
-import com.example.billing.service.entity.UserEntity;
 import com.example.billing.service.repository.UserTariffRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Component
 public class BalanceService {
     private final UserRepo userRepo;
     private final UserTariffRepo userTariffRepo;
@@ -56,47 +57,40 @@ public class BalanceService {
         return balance;
     }
 
-    @PostConstruct
-    private void init() {
-        var timer = new Timer();
-        var timerTask = new TimerTask() {
-            @Override
-            public void run() {
-                writeOffAmount();
-            }
-        };
-        timer.schedule(timerTask, 0, 60000);
-    }
-
-    //TODO Заюзать спринговый шедулер
+    @Scheduled(cron = "0 0/1 * * * *")
     private void writeOffAmount() {
         var allServices = StreamSupport.stream(userTariffRepo.findAll().spliterator(), false);
         var users = userRepo.findAll();
         for (UserEntity user : users
         ) {
 
-            if (user.getBalance() <= 0) {
-                log.info(String.format("[%s] Услуги приостановлены. Недостаточно средств", user));
-                continue;
+            try {
+                if (user.getBalance() <= 0) {
+                    log.info(String.format("[%s] Услуги приостановлены. Недостаточно средств", user));
+                    continue;
+                }
+
+                var activatedUserServices = allServices
+                        .filter(service -> Objects.equals(service.getUser().getId(), user.getId()) && service.getIsActivated()).toList();
+
+                var sum = activatedUserServices.stream()
+                        .map(service -> service.getTariff().getPricePerMinute())
+                        .mapToDouble(value -> value).sum();
+
+                var newBalance = user.getBalance() - sum;
+
+                user.setBalance(newBalance);
+
+                userRepo.save(user);
+
+                log.info(String.format("[%s] Списание средств %.2f", user, sum));
+
+                if (newBalance <= 0) {
+                    deactivateServices(activatedUserServices);
+                }
             }
-
-            var activatedUserServices = allServices
-                    .filter(service -> Objects.equals(service.getUser().getId(), user.getId()) && service.getIsActivated()).toList();
-
-            var sum = activatedUserServices.stream()
-                    .map(service -> service.getTariff().getPricePerMinute())
-                    .mapToDouble(value -> value).sum();
-
-            var newBalance = user.getBalance() - sum;
-
-            user.setBalance(newBalance);
-
-            userRepo.save(user);
-
-            log.info(String.format("[%s] Списание средств %.2f", user, sum));
-
-            if (newBalance <= 0) {
-                deactivateServices(activatedUserServices);
+            catch (RuntimeException e){
+                log.error(e.getMessage());
             }
         }
     }
